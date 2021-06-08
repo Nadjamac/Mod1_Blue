c7fbcb400cb75f587251157e61e16593e7ac9134
Nadja Machado <81447268+Nadjamac@users.noreply.github.com>
2021-06-07 21:23:17 -0300

    Add files via upload

---
+++ AL9-2.py
@@ -0,0 +1,11 @@
+
+
+# 2.Crie um código em Python para exibir a contagem de dígitos de um número inteiro positivo
+#  informado pelo usuário. 
+
+def contador(number):
+    contando=len(number)
+    return contando
+
+number=str(input("Informe um numero : \n"))
+print(contador(number))
